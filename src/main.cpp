ab0de2617efab4b101104accd32bf64f0606a578
Korarei <132204522+korarei@users.noreply.github.com>
2026-06-20 07:33:43 +0900

    Refactor: Use generated export header

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,8 @@
 #include <intern/wic/wic.hpp>
 #include <io/io.hpp>
 
+#include "api.h"
+
 #ifndef VERSION
 #define VERSION L"0.1.0"
 #endif
@@ -27,13 +29,13 @@ constinit COMMON_PLUGIN_TABLE info = {
 }  // namespace
 
 extern "C" {
-DWORD RequiredVersion() { return REQUIRES_AVIUTL2; }
+API DWORD RequiredVersion() { return REQUIRES_AVIUTL2; }
 
-void InitializeLogger(LOG_HANDLE* logger) { lut::aviutl::Logger::Init(logger); }
+API void InitializeLogger(LOG_HANDLE* logger) { lut::aviutl::Logger::Init(logger); }
 
-bool InitializePlugin(DWORD version) { return version >= RequiredVersion(); }
+API bool InitializePlugin(DWORD version) { return version >= RequiredVersion(); }
 
-void UninitializePlugin() {
+API void UninitializePlugin() {
     lut::filter::Deinit();
     lut::io::Deinit();
 
@@ -41,9 +43,9 @@ void UninitializePlugin() {
     lut::wic::WIC::Deinit();
 }
 
-COMMON_PLUGIN_TABLE* GetCommonPluginTable() { return &info; };
+API COMMON_PLUGIN_TABLE* GetCommonPluginTable() { return &info; }
 
-void RegisterPlugin(HOST_APP_TABLE* host) {
+API void RegisterPlugin(HOST_APP_TABLE* host) {
     lut::filter::Init(host);
     lut::io::Init(host);
 }
